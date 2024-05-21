98eef925bcbeb1b220613b84e4fc9d6e285c3d27
Secretaker34 <kyleteker@gmail.com>
2024-05-21 16:12:36 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
+const doNotDelete = "𝗞𝗬𝗟𝗘 𝗚𝗢𝗔𝗧𝗕𝗢𝗧 𓃵";
 /** 
 * @author NTKhang
 * @author: do not delete it
